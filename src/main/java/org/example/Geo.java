969b5cf628c66f1605d259c5b2909e1cb7c4d1e4
RazerFord <desk10567@gmail.com>
2024-05-12 10:45:30 +0300

    added new file

--- src/main/java/org/example/Geo.java
+++ src/main/java/org/example/Geo.java
@@ -2,21 +2,21 @@ package org.example;
 
 public class Geo {
     /* here is very important code */
-    public Point1 getPositionById(int id) {
-        Point1 point = points[id];
+    public Point3 getPositionById(int id) {
+        Point3 point = points[id];
         /* here is very important code */
 
         return point;
     }
 
-    private final Point1[] points = new Point1[1000];
+    private final Point3[] points = new Point3[1000];
 
     {
         for (int i = 0; i < points.length; i++) {
-            Point1 p = new Point1();
-            p.x(10);
-            p.y(20);
-            p.z(30);
+            Point3 p = new Point3();
+            p.setX(10);
+            p.setY(20);
+            p.setZ(30);
             points[i] = p;
         }
     }
---
+++ src/main/java/org/example/Point3.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.Objects;
+
+public class Point3 {
+    private int x;
+    private int y;
+    private int z;
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point3 point = (Point3) o;
+        return x == point.x && y == point.y && z == point.z;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, z);
+    }
+}
